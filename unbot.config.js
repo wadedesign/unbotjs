870feb0a5687dff5cd450935140442b3ee2c906a
wadder12 <apwade75009@gmail.com>
2024-03-12 17:23:52 -0500

    ✔ added pm2 confile file (unbot.config.js)

---
+++ unbot.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    name: "unbotjs",
+    script: "src/index.js",
+    interpreter: "~/.bun/bin/bun",
+  };
+  
+
+// how to run bun and pm2 together
+
+// bun guide https://bun.sh/guides/ecosystem/pm2
+
+// make sure bun is in the home directory // the interpreter is the path to the bun executable
+
+// if gives error prob rename back to pm2.config.js
