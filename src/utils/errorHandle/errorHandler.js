0bf2f96dd241879a26687498e3027311b84bca0d
wadder12 <apwade75009@gmail.com>
2024-03-14 21:07:15 -0500

    ✔ updated errorhandler to provide user with and id of the error ✔ made the embed for the errorhandler better

--- src/utils/errorHandle/errorHandler.js
+++ src/utils/errorHandle/errorHandler.js
@@ -1,23 +1,61 @@
 // utils/errorHandler.js
 import { EmbedBuilder } from 'discord.js';
+import fs from 'fs';
+import path from 'path';
+
+const generateErrorId = () => Date.now();
+
+const logErrorToFile = (errorId, error) => {
+    const logFilePath = path.join(__dirname, '../../logs/errorLog.json');
+    const errorLog = {
+        id: errorId,
+        message: error.message,
+        stack: error.stack,
+        timestamp: new Date().toISOString(),
+    };
+
+    fs.readFile(logFilePath, (readErr, data) => {
+        let logs = [];
+        if (!readErr) {
+            try {
+                logs = JSON.parse(data.toString());
+            } catch {
+            }
+        }
+        logs.push(errorLog);
+
+        fs.writeFile(logFilePath, JSON.stringify(logs, null, 2), writeErr => {
+            if (writeErr) {
+                console.error('Error writing to error log file:', writeErr);
+            }
+        });
+    });
+};
 
 /**
- * Handles errors and informs the user.
+ * Handles errors and informs the user with a visually improved embed message.
  * @param {Error} error The error that occurred.
  * @param {Interaction} interaction The interaction from which the error originated.
  */
 export async function handleError(error, interaction) {
     console.error('Error:', error);
 
-    // Optionally, log the error using a custom logging utility
-    // await logError(error);
+    const errorId = generateErrorId();
+    logErrorToFile(errorId, error);
 
     const embed = new EmbedBuilder()
-        .setTitle('Oops! Something went wrong 😢')
-        .setDescription('There was an issue processing your request. Please try again later.')
-        .setColor(0xFF0000); // Red color for error
+    .setTitle('🚨 An Error Occurred 🚨')
+    .setDescription('Oops! Something didnt work as expected. But dont worry, were on it.')
+    .addFields(
+        { name: 'Error ID', value: `\`${errorId}\``, inline: true },
+        { name: '\u200B', value: '\u200B', inline: true }, // This adds an empty field for spacing, if needed
+        { name: 'Immediate Steps', value: 'Please try again later. If the issue persists, reach out to our support team with the Error ID.', inline: false }
+    )
+    .setColor(0xDD2E44) // A more specific shade of red for errors
+    .setFooter({ text: 'Thank you for your patience!' })
+    .setTimestamp();
 
-    // Check if the interaction has already been replied to
+    // Send the embed based on the interaction state
     if (interaction.replied || interaction.deferred) {
         await interaction.followUp({ embeds: [embed], ephemeral: true }).catch(console.error);
     } else {
