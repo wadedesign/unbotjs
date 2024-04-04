ca804596a97e523a0690c616d3bac621f63e4b02
Andrew Wade <apwade75009@gmail.com>
2024-04-03 19:03:54 -0500

    :zap: Release Odyssey
    - updated env.example
    - added emoji config json file (alot easier to add emojis)
    - updated emojisUtils.js script to use json file

--- src/utils/importEmojis/emojiUtils.js
+++ src/utils/importEmojis/emojiUtils.js
@@ -1,14 +1,16 @@
-// src/utils/emojiUtils.js
-import dotenv from 'dotenv';
-dotenv.config();
+// src/utils/importEmojis/emojiUtils.js
+import fs from 'fs';
+import path from 'path';
+
+
+const emojiFilePath = path.join(__dirname, 'emojis.json');
+const emojiData = JSON.parse(fs.readFileSync(emojiFilePath, 'utf8'));
 
 /**
- * Fetches a custom emoji string from environment variables by name.
+ * Fetches a custom emoji string from a JSON file by name.
  * @param {string} name - The name of the emoji to fetch.
  * @returns {string} The emoji string if found, otherwise an empty string.
  */
-
 export function getCustomEmoji(name) {
-    const emoji = process.env[`${name.toUpperCase()}_EMOJI`];
-    return emoji || '';
+    return emojiData[name.toLowerCase()] || '';
 }
